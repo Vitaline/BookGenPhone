d2d0779e900f4ab8f3aede24cee0a4ed58300617
Vit <vitlik_07@mail.ru>
2018-09-16 15:16:40 +0500

    333333333

--- PhoneNumber.cs
+++ PhoneNumber.cs
@@ -1,11 +1,11 @@
 using System;
-class PhoneNumber {
+class PhoneNumber333333333 {
 	public PhoneNumber (string name , string number) {
 		Name = name;
 		222222222Number = number;
 	}
 	public Number {get; set;}
 
-	pub2222222222222lic Name {get; set;}
+	pub222222222333333333332222lic Name {get; set;}
 
 }
