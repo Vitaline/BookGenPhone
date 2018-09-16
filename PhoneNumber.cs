ac3a2a3f649bb680d1250ea637fcbf8faf597e69
Vit <vitlik_07@mail.ru>
2018-09-16 15:49:01 +0500

    Try to commit

--- PhoneNumber.cs
+++ PhoneNumber.cs
@@ -1,12 +1,14 @@
 using System;
-class PhoneNumber333333333 {
-	public PhoneNumber (string name , string number) {
+class PhoneNumber
+{
+	public PhoneNumber (string name , string number)
+	{
 		Name = name;
-		2222244444444442222Number = number;
+		Number = number;
 	}
 	public Number {get; set;}
 
 
-	pub22222244444444444442222222lic Name {get; set;}
+	public Name {get; set;}
 
 }
