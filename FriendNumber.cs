11385824778b029c5c816b71aa7fe3474aa4fa35
Vit <vitlik_07@mail.ru>
2018-09-16 23:03:30 +0500

    Error at 6 line

--- FriendNumber.cs
+++ FriendNumber.cs
@@ -3,7 +3,7 @@ class FriendNumber : PhoneNumber {
     public bool isWorkNumber {get;set;}
     public FriendNumber (string name, string nuber, bool isW) :base (name,number)
     {
-        siWorkNumber = isW;
+        isWorkNumber = isW;
     }
 
 }
