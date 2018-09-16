337631bad08bff46838ad0266a5b9353ba00e8e5
Vit <vitlik_07@mail.ru>
2018-09-16 14:23:11 +0500

    add property Name

--- PhoneNumber.cs
+++ PhoneNumber.cs
@@ -1,9 +1,11 @@
 using System;
 class PhoneNumber {
-public PhoneNumber (string name , string number) {
-Name = name;
-Number = number;
-}
-public Number {get; set;}
+	public PhoneNumber (string name , string number) {
+		Name = name;
+		Number = number;
+	}
+	public Number {get; set;}
+
+	public Name {get; set;}
 
 }
